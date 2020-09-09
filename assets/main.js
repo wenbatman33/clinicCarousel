6ebd06b63df8f4fd219dd75cb4ee8675af05ea09
batman <batman@flyup.tw>
2020-09-09 15:04:27 +0800

    update

--- assets/main.js
+++ assets/main.js
@@ -1,26 +1,63 @@
 $(function () {
 	var baseUrl = 'http://61.220.95.146:3000';
+	var roomDataList;
+	var roomDataLength;
 	var patientList;
+
 	var patientAll = 0;
 	var current = 0;
 	var renderItems = 5;
 	var timer;
 	var loopTime = 5000;
 
+	function getRoomData() {
+		// $.get("./assets/data_01.json", {}, function (res) {
+		$.post(baseUrl + '/Api/GeteEmployeeScheduleDataList', {}, function (res) {
+			showRoomData(res);
+		}).fail(function () {
+			$('.department').html('尚無資料');
+			$('.doctor').html('尚無資料');
+			$('.clinic').html('尚無資料');
+		});
+	}
 	function getData() {
-		$.ajax({
-			url: './assets/data_02.json',
-			method: 'get',
-		})
-			.done(function (res) {
-				step2(res);
-			})
-			.fail(function (err) {
-				console.log(err);
-			});
+		// $.get("./assets/data_02.json", {}, function (
+		$.post(baseUrl + '/Api/GetRegisteredButNotSeenDataList', {}, function (res) {
+			showData(res);
+		}).fail(function () {
+			var element = '';
+			element += '<li class="noData">';
+			element += '<span>尚無資料</span>';
+			element += '</li>';
+			$('.patientUL').html(element);
+		});
 	}
 
-	function step2(res) {
+	function showRoomData(res) {
+		roomDataList = res.data;
+		roomDataLength = res.data.length;
+		var currentTime = dayjs().format('YYYY/MM/DD HH:mm:ss');
+		var today = dayjs().format('YYYY/MM/DD');
+
+		for (j = 0; j < roomDataLength; j++) {
+			var sTime = today + ' ' + roomDataList[j].SHI_TIME_S + ':00';
+			var eTime = today + ' ' + roomDataList[j].SHI_TIME_E + ':59';
+			if (dayjs(currentTime).isAfter(dayjs(sTime)) && dayjs(currentTime).isBefore(dayjs(eTime))) {
+				var tiemGap = dayjs(eTime).valueOf() - Date.now();
+				// 緩衝65秒
+				tiemGap += 65000;
+				setTimeout(() => {
+					getRoomData();
+				}, tiemGap);
+
+				$('.department').html(roomDataList[j].SEC_SENAME);
+				$('.doctor').html(roomDataList[j].EMP_EMPNAME);
+				$('.clinic').html(roomDataList[j].ROM_RONAME);
+			}
+		}
+	}
+
+	function showData(res) {
 		patientList = res.data;
 		patientAll = res.data.length;
 		loop(current);
@@ -53,4 +90,5 @@ $(function () {
 	}
 
 	getData();
+	getRoomData();
 });
