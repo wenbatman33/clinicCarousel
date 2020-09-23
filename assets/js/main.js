2818fe7f4abd7b4bd03d635d2528d6403f2a5cfd
batman <batman@flyup.tw>
2020-09-23 23:14:33 +0800

    註解事件流

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,6 +11,7 @@ $(function () {
 	var showInfo = {};
 	var timer = '';
 	var loopTime = 5000;
+
 	function reSetVar() {
 		ROOM_NUM = 0;
 		switchRoom = 0;
@@ -19,11 +20,11 @@ $(function () {
 		roomList = [];
 		patLength = [];
 		maxLength = 0;
-		renderItems = 5;
 		showInfo = {};
 	}
 	function noneData() {
 		// 沒有患者時的顯示方式 30秒後再取資料
+		console.log('目前休診中');
 		var element = '';
 		element += '<li class="noData">目前休診中</li>';
 		$('.patientUL').html(element);
@@ -50,8 +51,6 @@ $(function () {
 			dataType: 'json',
 			contentType: 'application/json;charset=utf-8',
 			success: function (res) {
-				console.log(res);
-				console.log(res.data.length);
 				if (res.data.length > 0) {
 					init(res);
 				} else {
@@ -74,35 +73,24 @@ $(function () {
 	function setup() {
 		loop();
 	}
+
 	function loop() {
 		switchRoom = ROOM_NUM % rooms;
 		showInfo.room = roomList[switchRoom];
 		showInfo.pat = [];
+		// 依序讀取各診間看診者名單
 		for (var i = 0; i < renderItems; i++) {
 			var temp = roomList[switchRoom].pat[currentPatNumber[switchRoom] + i];
 			if (temp) {
 				showInfo.pat.push(temp);
 			}
 		}
-		$('.department').html(showInfo.room.SEC_SENAME);
-		$('.doctorName').html(showInfo.room.EMP_EMPNAME);
-		$('.doctorJobTitle').html('醫師');
-		$('.clinicName').html(showInfo.room.SHI_EASYNAME);
-		$('.roomName').html(showInfo.room.ROM_RONAME);
-
-		var element = '';
-		for (var j = 0; j < showInfo.pat.length; j++) {
-			if (showInfo.pat[j]) {
-				element += '<li class="patientItem">';
-				element += '<span class="id">' + showInfo.pat[j].OCB_VISITNO + '</span>';
-				element += '<span class="name">' + showInfo.pat[j].PT_PATNAME + '</span>';
-				element += '</li>';
-			}
-		}
-		$('.patientUL').html(element);
+		showInfoData();
+		checkNextStep();
+	}
+	function checkNextStep() {
 		currentPatNumber[switchRoom] += 5;
 		ROOM_NUM += 1;
-
 		if (currentPatNumber[switchRoom] > patLength[switchRoom]) {
 			currentPatNumber[switchRoom] = 0;
 		}
@@ -115,6 +103,23 @@ $(function () {
 				loop();
 			}, loopTime);
 		}
+	}
+	function showInfoData() {
+		$('.department').html(showInfo.room.SEC_SENAME);
+		$('.doctorName').html(showInfo.room.EMP_EMPNAME);
+		$('.doctorJobTitle').html('醫師');
+		$('.clinicName').html(showInfo.room.SHI_EASYNAME);
+		$('.roomName').html(showInfo.room.ROM_RONAME);
+		var element = '';
+		for (var j = 0; j < showInfo.pat.length; j++) {
+			if (showInfo.pat[j]) {
+				element += '<li class="patientItem">';
+				element += '<span class="id">' + showInfo.pat[j].OCB_VISITNO + '</span>';
+				element += '<span class="name">' + showInfo.pat[j].PT_PATNAME + '</span>';
+				element += '</li>';
+			}
+		}
+		$('.patientUL').html(element);
 	}
 	// /////////////
 	getData();
